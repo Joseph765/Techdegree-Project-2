f8fa400a44884f91277bfdb31ec28384ca6e7938
Joseph Chauvin <joseph.chauvin102@gmail.com>
2019-07-03 17:32:55 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,25 +1,53 @@
 const studentListItems = document.querySelectorAll('li.student-item'); //two global variables
 const pageNumberLimit = 10;
-
-//this loop hides all student lists
-for (let i = 0; i < studentListItems.length; i += 1) {
-  studentListItems[i].style.display = 'none';
-}
+const studentName = document.querySelectorAll('h3');
 
 // this function displays according to the list length on the page, and the page number
+
+function hideAllStudents() {  //this function hides all students
+  for (let i = 0; i < studentListItems.length; i += 1) { //this loop hides all student lists
+    studentListItems[i].style.display = 'none';
+  }
+}
 
 function showPage(list, page) {
   const startIndex = (page * list) - list;
   const endIndex = page * list;
+  hideAllStudents();
   for(let i = startIndex; i < endIndex; i += 1) {
     studentListItems[i].style.display = 'block';
   }
 }
 
+function addSearchBar() {
+  const parentDiv = document.querySelector('div.page-header');
+  const div = document.createElement('div');
+  parentDiv.appendChild(div);
+  div.className = 'student-search';
+  const input = document.createElement('input');
+  div.appendChild(input);
+  input.placeholder = 'Search for students...';
+  const button = document.createElement('button');
+  div.appendChild(button);
+  button.textContent = 'Search';
+  button.addEventListener('click', (e) => {
+    hideAllStudents();
+    let searchResults = [];
+    for (let i = 0; i < studentListItems.length; i += 1) {
+      if (studentName[i].textContent.toUpperCase().includes(input.value.toUpperCase()) ) {
+        studentName[i].style.display = 'block';
+        searchResults.push(studentListItems[i]);
+      }
+      showPage(pageNumberLimit, 1);
+      appendPageLinks(searchResults);
+    }
+  });
+}
+
 // this function appends the links to the DOM and adds functionality
 
 function appendPageLinks(list) {
-  const numberOfPages = list.length / pageNumberLimit;
+  const numberOfPages = (list.length / pageNumberLimit) + 1;
   const parentDiv = document.querySelector('div.page');
   const div = document.createElement('div');
   div.className = "pagination";
@@ -33,21 +61,18 @@ function appendPageLinks(list) {
     li.appendChild(a);
     a.textContent = i;
     a.href = "#";
+  }
     ul.addEventListener('click', (e) => {
-      if (e.target.tagName === 'a') {
+        const a = ul.querySelectorAll('a');
+        for (let i = 0; i < a.length; i += 1) {
+          a[i].className = '';
+        }
         const activeLink = e.target;
         activeLink.className = "active";
-      }
+        showPage(pageNumberLimit, parseInt(activeLink.textContent));
     });
   }
-}
 
 appendPageLinks(studentListItems);
-
-
-
-
-/***
-   Create the `appendPageLinks function` to generate, append, and add
-   functionality to the pagination buttons.
-***/
+showPage(pageNumberLimit, 1); //calling the function to display the first page initially
+addSearchBar();
